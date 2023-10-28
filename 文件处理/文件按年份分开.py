fbc4b9e9ea021920a52f72e89cab1c849aba9b89
Yishiyork <3051176425@qq.com>
2023-10-28 13:17:50 +0800

    初始增加

---
+++ 文件处理/文件按年份分开.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+# 输入文件夹路径
+folder_path = "E:\Source_for_sale\A股年报 PDF+TXT\A股年报TXT [56081份18.8GB] - 副本"
+
+# 遍历文件夹中的txt文件
+for filename in os.listdir(folder_path):
+    if filename.endswith(".txt"):
+        # 提取年份标记
+        year = filename[7:11]
+
+        # 创建对应年份的文件夹（如果不存在）
+        year_folder = os.path.join(folder_path, year)
+        if not os.path.exists(year_folder):
+            os.mkdir(year_folder)
+
+        # 构建源文件路径和目标文件路径
+        source_file = os.path.join(folder_path, filename)
+        target_file = os.path.join(year_folder, filename)
+
+        # 移动文件到对应年份的文件夹
+        shutil.move(source_file, target_file)
+
+print("文件移动完成")
